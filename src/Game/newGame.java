ba67d87aa1cb182996e50ce9f3ad008a3ba99f6f
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2021-06-05 18:02:44 +0700

    Update 20 folder images

--- src/Game/newGame.java
+++ src/Game/newGame.java
@@ -2,8 +2,9 @@ package Game;
 
 public class newGame {
 	public newGame() {
-		int N = 8;
+		int N = 20;
 		int M = (int) (Math.random() * (N - 1) + 1);
+//		M = 20;
 		new OriginalIm(M);
 		new PuzzleGame(M);
 	}
